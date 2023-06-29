38a34c45f98d1c901d639e5c0ae983315261ac65
Kang MinSeok <alstjr8125@naver.com>
2023-06-29 10:13:29 +0900

    last commit

--- js/bt_tog.js
+++ js/bt_tog.js
@@ -1,12 +1,12 @@
 const toggle_lo_bt = document.querySelector(".fixed_bu_lo");
-// const toggle_target_ha = document.querySelector(".course_ha");
+const toggle_target_ha = document.querySelector(".course_ha");
 const toggle_target_a = document.querySelector(".course_a");
 const toggle_target_b = document.querySelector(".course_b");
 const toggle_target_night = document.querySelector(".course_night");
 // const toggle_target_night_JD = document.querySelector(".course_night_JD");
 
 const toggle_tt_bt = document.querySelector(".fixed_bu_tt");
-// const toggle_target_ha_tt = document.querySelector(".course_ha_tt");
+const toggle_target_ha_tt = document.querySelector(".course_ha_tt");
 const toggle_target_c = document.querySelector(".course_c");
 const toggle_target_d = document.querySelector(".course_d");
 const toggle_target_night_tt = document.querySelector(".course_night_tt");
@@ -20,7 +20,7 @@ const click_col = "click_col";
 
 function handlebtClick_lo() {
   toggle_tt_bt.classList.remove(click_col);
-  // toggle_target_ha_tt.classList.add(bt_hidden);
+  toggle_target_ha_tt.classList.add(bt_hidden);
   toggle_target_c.classList.add(bt_hidden);
   toggle_target_d.classList.add(bt_hidden);
   toggle_target_night_tt.classList.add(bt_hidden);
@@ -33,7 +33,7 @@ function handlebtClick_lo() {
   toggle_target_a.classList.remove(bt_hidden);
   toggle_target_b.classList.remove(bt_hidden);
   toggle_target_night.classList.remove(bt_hidden);
-  // toggle_target_ha.classList.remove(bt_hidden);
+  toggle_target_ha.classList.remove(bt_hidden);
   // toggle_target_night_JD.classList.remove(bt_hidden);
 }
 function handlebtClick_tt() {
@@ -41,7 +41,7 @@ function handlebtClick_tt() {
   toggle_target_a.classList.add(bt_hidden);
   toggle_target_b.classList.add(bt_hidden);
   toggle_target_night.classList.add(bt_hidden);
-  // toggle_target_ha.classList.add(bt_hidden);
+  toggle_target_ha.classList.add(bt_hidden);
   // toggle_target_night_JD.classList.add(bt_hidden);
 
   toggle_t_bt.classList.remove(click_col);
@@ -49,7 +49,7 @@ function handlebtClick_tt() {
   toggle_target_Q.classList.add(bt_hidden);
 
   toggle_tt_bt.classList.add(click_col);
-  // toggle_target_ha_tt.classList.remove(bt_hidden);
+  toggle_target_ha_tt.classList.remove(bt_hidden);
   toggle_target_c.classList.remove(bt_hidden);
   toggle_target_d.classList.remove(bt_hidden);
   toggle_target_night_tt.classList.remove(bt_hidden);
@@ -60,11 +60,11 @@ function handlebtClick_t() {
   toggle_target_a.classList.add(bt_hidden);
   toggle_target_b.classList.add(bt_hidden);
   toggle_target_night.classList.add(bt_hidden);
-  // toggle_target_ha.classList.add(bt_hidden);
+  toggle_target_ha.classList.add(bt_hidden);
   // toggle_target_night_JD.classList.add(bt_hidden);
 
   toggle_tt_bt.classList.remove(click_col);
-  // toggle_target_ha_tt.classList.add(bt_hidden);
+  toggle_target_ha_tt.classList.add(bt_hidden);
   toggle_target_c.classList.add(bt_hidden);
   toggle_target_d.classList.add(bt_hidden);
   toggle_target_night_tt.classList.add(bt_hidden);
